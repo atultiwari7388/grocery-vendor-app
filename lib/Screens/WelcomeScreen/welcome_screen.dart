c08f58aed8ee2c70cb54a0a0f7ecfd19c6ece96c
Atul Tiwari <tiwariatul9526@gmail.com>
2021-08-29 23:35:30 +0530

    create bottom sheet

--- lib/Screens/WelcomeScreen/welcome_screen.dart
+++ lib/Screens/WelcomeScreen/welcome_screen.dart
@@ -10,6 +10,91 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  // ignore: unused_field
+  bool _validPhoneNumber = false;
+
+  void showBottomSheet(context) {
+    showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+      ),
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, StateSetter myState) {
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "LOGIN",
+                    style: GoogleFonts.aBeeZee(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Enter Your Phone Number to Process",
+                    style: GoogleFonts.aBeeZee(),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixText: "+91",
+                      prefixStyle: GoogleFonts.aBeeZee(
+                        color: Colors.grey,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      labelText: "10 digit phone number",
+                    ),
+                    keyboardType: TextInputType.phone,
+                    maxLength: 10,
+                    autofocus: true,
+                    onChanged: (value) {
+                      if (value.length == 10) {
+                        myState(() {
+                          _validPhoneNumber = true;
+                        });
+                      } else {
+                        myState(() {
+                          _validPhoneNumber = false;
+                        });
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: AbsorbPointer(
+                        absorbing: _validPhoneNumber ? false : true,
+                        child: ElevatedButton(
+                          child: Text(
+                            _validPhoneNumber
+                                ? "Continue"
+                                : "Ennter Your Number",
+                          ),
+                          onPressed: () {},
+                        ),
+                      ))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,14 +104,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: [
             Column(
               children: [
-                Positioned(
-                  right: 0.0,
-                  top: 10.0,
-                  child: TextButton(
-                    child: Text("Skip"),
-                    onPressed: () {},
-                  ),
-                ),
                 Expanded(child: OnBoardScreen()),
                 Text(
                   "Order from your nearest shop ?",
@@ -40,9 +117,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     style: GoogleFonts.aBeeZee(),
                   ),
                 ),
-                SizedBox(height: 20),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showBottomSheet(context);
+                  },
                   child: RichText(
                     text: TextSpan(
                       text: "Already a Customer ? ",
