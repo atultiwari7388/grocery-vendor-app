bb2222e8f4d1f9b4a17bc7a32d1a32e56a138dd1
Atul Tiwari <tiwariatul9526@gmail.com>
2021-08-29 19:47:23 +0530

    create splash screen

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_vendor_app/Screens/Splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
